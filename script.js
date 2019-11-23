d04b5e139bee1cc36658680763ba54d21cbe5852
nazkin <nazar.kinash93@gmail.com>
2019-11-23 18:32:41 -0500

    major functionality done

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
 
 $( document ).ready(function() {
+
+    var schedArr = [];
+    if(localStorage.getItem("schedule")) {
+        schedArr = JSON.parse(localStorage.getItem("schedule"));
+    } else {
+        schedArr = [""];
+        localStorage.setItem("schedule", JSON.stringify(schedArr))
+        schedArr = JSON.parse(localStorage.getItem("schedule"));
+    }
     
-    //API KEY FOR WEATHER
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=c7653ece574413d1f94171d5a4d625df",
-    function(data) {
-        console.log(data);
-        var temp =data.main.temp;
-        console.log(`Todays weather in toronto is ${temp}`);
-    });
-    //********************************************************** */
-        
-    
+    //Display the time and the hour of the day
+    var timeNow = moment().format('LT'); 
+    $(".timeofDay").text(timeNow);
+    var hourNow = moment().hour();
+    console.log(hourNow);
 
     $('ul').on('click','li', function() {
         $(this).toggleClass("completed");
@@ -36,20 +40,42 @@ $( document ).ready(function() {
         $("ul").toggleClass("remove-todo");
     });
 
-console.log(moment().subtract(10, 'days').calendar());
+
 
 var insertSchedule = function() {
-    var htmlComponent,newHtml, time;
+    var htmlComponent,newHtml;
     
-    htmlComponent = '<div id = "schedule" class = "col-12 p-0 my-2"><div id = "each-hour" class="row p-0 m-0 "><div class="exact-hour col-2 d-flex align-items-center justify-content-center ">%time%:00</div><div class="hourly-input col-9"><input class="sched-input" type="text" placeholder="Enter your new plan"></div><div class="delete-input col-1 d-flex justify-content-center"><button type="button" class="btn btn-outline-danger">Delete</button> </div></div></div>';
+    htmlComponent = '<div id = "schedule" class = "col-12 p-0 my-2"><div id = "each-hour" class="row p-0 m-0 "><div class="%exact-hour% col-2 d-flex align-items-center justify-content-center ">%time%:00</div><div class="hourly-input col-9"><input id="%id%" class="sched-input" type="text" placeholder="Enter your new plan" value="%value%" ></div><div class="delete-input col-1 d-flex justify-content-center"><button type="button" class="btn btn-outline-danger">Delete</button> </div></div></div>';
     for(var x = 7; x < 24; x++) {
         newHtml = htmlComponent.replace("%time%",x);
-        $("#main").append(newHtml);  
+        newHtml = newHtml.replace("%id%", x-7)
+        newHtml = newHtml.replace("%value%",schedArr[x-7]); //since the counter starts at 7 i subtract 7 to get it down to 0 for the array reference
+         if(x<hourNow) {
+            newHtml = newHtml.replace("%exact-hour%","exact-hour1");
+        }else if(x > hourNow) {
+            newHtml = newHtml.replace("%exact-hour%","exact-hour3");
+        }else {
+            newHtml = newHtml.replace("%exact-hour%","exact-hour2");
+        }
+        $("#main").append(newHtml);
+
     }
- 
+  
 }
 
 insertSchedule();
+
+//Add an event listener to my box and look for an id value and fill that info into my array
+    $('#main').on('keypress', function(event){
+        if(event.keyCode === 13){
+          schedArr[event.target.id] = event.target.value;
+          localStorage.setItem("schedule", JSON.stringify(schedArr));
+          schedArr = JSON.parse(localStorage.getItem("schedule"));
+          event.target.blur();
+        }
+        
+    
+    });
 
 });
 
